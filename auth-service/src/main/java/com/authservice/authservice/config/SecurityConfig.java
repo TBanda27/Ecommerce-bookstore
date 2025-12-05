20ecc51935b2619d59b7df202209ecec5180ec5b
TBanda27 <tzbanda@outlook.com>
2025-12-05 21:37:34 +0000

    security config update

--- auth-service/src/main/java/com/authservice/authservice/config/SecurityConfig.java
+++ auth-service/src/main/java/com/authservice/authservice/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
     }
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+
         return config.getAuthenticationManager();
     }
 }
